b4d02eb6103bf716b8a620870ea42af07d308eb1
Max <mbremer@richard-mueller.de>
2017-09-21 13:59:42 +0200

    Initial

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 /* eslint-env node */
 'use strict';
 
+const component = require('./lib/components');
+
 module.exports = {
-  name: 'ember-clean-project'
+    name: 'ember-clean-project',
+    includedCommands: function() {
+        return {
+            cleanComponentsCommand: {
+                name: 'clear:components',
+                description: 'Shows unused components',
+                run: function(commandOptions, rawArgs) {
+                    component.clearComponents();
+                }
+            },
+            cleanCssCommand: {
+                name: 'clean:css',
+                description: 'A test command that says hello',
+                availableOptions: [{
+                    name: 'format',
+                    type: String,
+                    default: 'lowercase',
+                    aliases: ['f']
+                }],
+                run: function(commandOptions, rawArgs) {
+                    if (commandOptions.format === 'uppercase') {
+                        console.log('HELLO!');
+                    } else {
+                        console.log('hello!');
+                    }
+                }
+            }
+        }
+    }
 };
